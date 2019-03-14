31a8467a577028a247acc28161cb72b92bf6d7ed
vchannabasav <basava.vasi@gmail.com>
2019-03-14 21:14:58 +0530

    mobile navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -98,4 +98,18 @@ $(document).ready(function() {
     },
     { offset: "50%" }
   );
+
+  // Mobile navigation
+  $(".js--nav-icon").click(function() {
+    var nav = $(".js--main-nav");
+    nav.slideToggle(200);
+    var icon = $(".js--nav-icon i");
+    if (icon.hasClass("ion-navicon-round")) {
+      icon.removeClass("ion-navicon-round");
+      icon.addClass("ion-close-circled");
+    } else {
+      icon.removeClass("ion-close-circled");
+      icon.addClass("ion-navicon-round");
+    }
+  });
 });
